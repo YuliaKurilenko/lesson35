5ca2cfe65c2b36e691c4865673a87a167e425b95
YuliaKurilenko <162790631+YuliaKurilenko@users.noreply.github.com>
2024-08-28 18:27:49 +0300

    Add files via upload

---
+++ database.py
@@ -0,0 +1,33 @@
+import sqlite3
+
+connection = sqlite3.connect('not_telegram.db')
+cursor = connection.cursor()
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS Users(
+id INTEGER PRIMARY KEY,
+username TEXT NOT NULL,
+email TEXT NOT NULL,
+age INTEGER,
+balance INTEGER NOT NULL
+)
+''')
+# cursor.execute('CREATE INDEX IF NOT EXISTS idx_email ON Users(email)')
+
+for i in range(1, 11):
+    cursor.execute('INSERT INTO Users(username,email,age,balance) VALUES (?,?,?,?)',(f'User{i}', f'example{i}@gmail.com', f'{i*10}', '1000'))
+for i in range(1, 11, 2):
+    cursor.execute('UPDATE Users SET balance = ? WHERE id = ?', (500, i))
+for i in range(1, 11, 3):
+    cursor.execute('DELETE FROM Users WHERE id = ?', (i,))
+cursor.execute('SELECT username, email, age, balance FROM Users WHERE age != 60')
+users = cursor.fetchall()
+my_table = ['Имя: ', ' | Почта: ', ' | Возраст: ', ' | Баланс: ']
+for user in users:
+    for i in range(len(user)):
+        print(my_table[i], end='')
+        print(user[i], end='')
+    print()
+# for user in users:
+#     print('Имя:',' | Почта:', ' | Возраст:', ' | Баланс:')
+connection.commit()
+connection.close()
